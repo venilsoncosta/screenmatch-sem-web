658c37b740a646db68ea3bd720d599d0e6d6c8e2
venilson <venilsoncosta21@gmail.com>
2024-01-04 09:24:17 -0300

    Trabalhando com datas, formatação e criando listas mais organizadas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -3,9 +3,13 @@ package br.com.alura.screenmatch.principal;
 import br.com.alura.screenmatch.model.DadosEpisodios;
 import br.com.alura.screenmatch.model.DadosSerie;
 import br.com.alura.screenmatch.model.DadosTemporada;
+import br.com.alura.screenmatch.model.Episodio;
 import br.com.alura.screenmatch.service.ConsumoApi;
 import br.com.alura.screenmatch.service.ConverteDados;
+import org.springframework.format.annotation.DateTimeFormat;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -38,28 +42,51 @@ public class Principal {
                 DadosTemporada dadosTemporada = conversor.obterdados(json, DadosTemporada.class);
                 temporadas.add(dadosTemporada);
             }
-            temporadas.forEach(System.out::println);
+//            temporadas.forEach(System.out::println);
 
-            for(int i = 0; i < dados.totalTemporadas(); i++){
-                List<DadosEpisodios> episodios = temporadas.get(i).episodios();
-                for(int j = 0; j < episodios.size(); j++){
-                    System.out.println(episodios.get(j).titulo());
-                }
-            }
+//            for(int i = 0; i < dados.totalTemporadas(); i++){
+//                List<DadosEpisodios> episodios = temporadas.get(i).episodios();
+//                for(int j = 0; j < episodios.size(); j++){
+//                    System.out.println(episodios.get(j).titulo());
+//                }
+//            }
 
-            temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//            temporadas.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+//
+//            List<DadosEpisodios> dadosEpisodios = temporadas.stream()
+//                    .flatMap(t -> t.episodios().stream())
+//                    .collect(Collectors.toList());
+//
+//            System.out.println("\nTop 5 episódios");
+//
+//            dadosEpisodios.stream()
+//                    .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+//                    .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
+//                    .limit(5)
+//                    .forEach(System.out::println);
 
-            List<DadosEpisodios> dadosEpisodios = temporadas.stream()
-                    .flatMap(t -> t.episodios().stream())
-                    .collect(Collectors.toList());
+            List<Episodio> episodios =  temporadas.stream()
+                    .flatMap(t -> t.episodios().stream()
+                    .map(d -> new Episodio(t.numero(), d))
+                    ).collect(Collectors.toList());
+            episodios.forEach(System.out::println);
 
-            System.out.println("\nTop 5 episódios");
+            System.out.println("A partir de que ano você quer ver os episódios? ");
+            var ano = leitura.nextInt();
+            leitura.nextLine();
 
-            dadosEpisodios.stream()
-                    .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
-                    .sorted(Comparator.comparing(DadosEpisodios::avaliacao).reversed())
-                    .limit(5)
-                    .forEach(System.out::println);
+            LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+            DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+            episodios.stream()
+                    .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                            .forEach(e -> System.out.println(
+                                    "Temporada: " + e.getTemporada() +
+                                            "- Episódio: " + e.getTitulo() +
+                                            "- Data de lançamento: " + e.getDataLancamento().format(formatador)
+                            ));
+
 
             // Perguntar ao usuário se ele deseja continuar
             System.out.println("Deseja buscar outra série? (s/n)");
